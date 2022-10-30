ad28270f9d9c65b0e905a58759d129a1da51af48
Elbashir Saror <53924829+elbashirsaror@users.noreply.github.com>
2022-10-31 00:17:14 +0800

    Update CircuitBreakerController.java

--- SpringBoot/CircuitBreaker/CircuitBreakerController.java
+++ SpringBoot/CircuitBreaker/CircuitBreakerController.java
@@ -1,1 +1,30 @@
-
+// copy this code to your controller and past it in the method where you want to have the circuit breaker
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+public class CircuitBreakerController {
+  
+  /**** resilience4j****/
+	@CircuitBreaker(name = "myCircuitBreaker", fallbackMethod = "testMethodfallback") // circuitbreaker name same as in application.properties
+	@RateLimiter(name = "myCircuitBreaker")
+	@Bulkhead(name = "myCircuitBreaker", fallbackMethod = "testMethodfallback") // fallback method should return the same entity as the original method
+	@Retry(name = "myCircuitBreaker")	
+	
+	@GetMapping("/testCircuitBreaker")
+  public ResponseEntity<?> testMethod() {
+    boolean serverUp = true; // use this to simulate a server
+    if(serverUUp){
+       return ResponseEntity.ok().body("worked");
+    }
+    return ResponseEntity.badRequest().body(null);
+  }
+  /******** the fallback methods ************/
+  public  ResponseEntity<?> testMethodfallback(CallNotPermittedException e) {
+		System.out.println("server is down! please try again after some time");
+		return ResponseEntity.internalServerError().body(null);
+	}
+	public  ResponseEntity<?> testMethodfallback(Exception e) {
+		System.out.println("server is down! please try again after some time");
+		return ResponseEntity.internalServerError().body(null);
+	}
+}
